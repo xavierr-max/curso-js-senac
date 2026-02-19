d0fad489841b01f547e6e7e1612e497464fc882a
MAXWELL DE ARAUJO XAVIER <a92291463@rn.senac.local>
2026-02-19 16:57:37 -0300

    19022026v1

---
+++ aulas-js/a01.js
@@ -0,0 +1,34 @@
+// let idade = 19;
+// let nota = 2.5;
+// let ativo = true;
+// let desativado = false;
+
+// console.log(nome);
+
+// if(true)
+// {
+//     nome = "vascogama"
+//     console.log(nome)
+// }
+
+// let name = "Maxwell";
+// let num = 19;
+// let bool = true;
+// let x;
+// // let y = asdf3323;
+
+
+// console.log(typeof(name));
+// console.log(typeof(num));
+// console.log(typeof(bool));
+// console.log(typeof(x));
+// console.log(typeof(y));
+
+// let n1 = 3.5;
+// let n2 = 5.8;
+// let r = (n1 + n2)/2;
+// console.log(`Mikael tirou a nota: ${r}`);
+
+// alert("olá, mundo")
+
+let nome = "Douglas"
