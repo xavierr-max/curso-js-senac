5c998a806bf003c752aea4b5e845bfebf5bbc753
xavierr-max <maxwelldearaujo1017@gmail.com>
2026-03-04 18:42:42 -0300

    04032026v1h

---
+++ Csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var teste = new Imc(0, 1.85);
+        var result = teste.ToCalculate();
+
+        Console.WriteLine(Math.Round(result, 2));
+    }
+}
--- csharp/Main.cs
+++
@@ -1,7 +0,0 @@
-namespace Main;
-
-public class Main(string[] args)
-{
-    var teste = new Imc(80, 185);
-    teste.ToCalculate();
-}
