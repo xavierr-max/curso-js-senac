d6edcc736b0cf365cb204092fec82f006ce1a375
MAXWELL DE ARAUJO XAVIER <a92291463@rn.senac.local>
2026-03-10 16:53:38 -0300

    10032026v2

---
+++ aulas/for/aula01.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i < 5; i++)
+// {
+//     console.log("Repetição: " + i)
+// }
+
+let numeros = [1, 2, 4, 8, 12]
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++)
+{
+    soma += numeros[i]
+}
+
+console.log(soma)
